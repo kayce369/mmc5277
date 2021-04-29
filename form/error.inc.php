c58ad1f36679bd790c94a0fca6913ed36312cf29
Kayce Georgi <georgi.kayce@ufl.edu>
2021-04-28 22:32:46 -0400

    updated form content to match main pages

--- form/error.inc.php
+++ form/error.inc.php
@@ -5,51 +5,146 @@
   border: 0;
 }
 
-body {
-  height: 100vh;
-  background: #F1F1F1;
+/* =============================================
+
+Navigation Styling
+
+===============================================*/
+
+header {
+  padding: 1% 10%;
+  background-color: #8B1F41;
+  position: relative;
+}
+
+header img {
+  margin: 5px 0 0;
+}
+
+header nav {
+  float: right;
+  position: absolute;
+  top: 40%;
+  right: 10%;
+}
+
+header nav ul li {
+  display: inline-block;
+  list-style: none;
+}
+
+header nav ul li a {
+  text-decoration: none;
+  color: white;
+  margin: 0 30px;
+  transition: .3s;
+  font-size: 14pt;
+}
+
+/* =============================================
+
+Univeresal
+
+===============================================*/
+
+main {
+  min-height: calc(100vh - 157px);
+  width: 75%;
+  margin: 0 auto;
+}
+
+h1,
+h2,
+h3,
+h4 {
+  font-family: 'Source Code Pro',monospace;
+}
+
+h1,
+h2,
+h3,
+p {
+  color: #200d13;
 }
 
 h1 {
-  font-family: 'Source Code Pro', monospace;
+margin: 20px 0;
   font-size: 36pt;
-  padding: 20px 0;
+  font-weight: bold;
+}
+
+main p {
+  margin: 5% 0;
+  font-size: 12pt;
 }
 
 p,
-li {
+a,
+li,
+.label_input_wrap {
   font-family: 'Lato', sans-serif;
-  font-size: 14pt;
-  line-height: 2;
 }
 
-a {
-  color: #0021A5;
+main a {
   text-decoration: none;
+  color: #200d13;
   transition: .3s;
 }
 
 a:hover {
-  text-decoration: underline 2;
-  text-decoration-color: #0021A5;
+  border-bottom: solid 2pt #0021A5;
   transition: .3s;
 }
 
 a:active {
-  text-decoration: underline 2;
-  text-decoration-color: #E87722;
+  border-bottom: solid 2pt #E87722;
   transition: .3s;
 }
 
-.container {
-  width: 75%;
-  margin: auto;
+/* =============================================
+
+Footer Styling
+
+===============================================*/
+
+footer {
+  background-color: #8B1F41;
+}
+
+footer ul {
+  text-align: center;
+}
+
+footer ul li {
+  display: inline-block;
+  list-style: none;
+  color: white;
+  margin: 1% 10%;
+}
+
+footer ul li a {
+  color: white;
+  text-decoration: none;
+  transition: .3s;
 }
 
 </style>
 
-<div class="container">
-	<h1>Missing fields</h1>
+</head>
+<body>
+  <header>
+    <a href="index.html"><img src="images/logo.png" alt="kg logo" width="75" /></a>
+    <nav>
+      <ul>
+        <li><a href="work.html">Work</a></li>
+        <li><a href="resume.html">R&eacute;sum&eacute;</a></li>
+        <li><a href="contact.html">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+
+<main>
+  <h1>Missing fields</h1>
 	<p>Sorry, you have not completed all of the required fields.</p>
 	<p>Please hit <a href="#" onClick="history.go(-1)">back</a> and complete the following required fields.</p>
 
@@ -60,6 +155,12 @@ a:active {
 		}
 	?>
 	</ul>
+</main>
 
-	<p><strong><a href="#" onClick="history.go(-1)">Back to form</a></strong></p>
-</div>
+<footer>
+  <ul>
+    <li><a href="mailto:georgi.kayce@ufl.edu">georgi.kayce@ufl.edu</a></li>
+    <li><a href="tel:2405294709">(240) 529-4709</a></li>
+    <li>Washington, DC</li>
+  </ul>
+</footer>
--- form/thankyou.inc.php
+++ form/thankyou.inc.php
@@ -5,50 +5,153 @@
   border: 0;
 }
 
-body {
-  height: 100vh;
-  background: #F1F1F1;
+/* =============================================
+
+Navigation Styling
+
+===============================================*/
+
+header {
+  padding: 1% 10%;
+  background-color: #8B1F41;
+  position: relative;
+}
+
+header img {
+  margin: 5px 0 0;
+}
+
+header nav {
+  float: right;
+  position: absolute;
+  top: 40%;
+  right: 10%;
+}
+
+header nav ul li {
+  display: inline-block;
+  list-style: none;
+}
+
+header nav ul li a {
+  text-decoration: none;
+  color: white;
+  margin: 0 30px;
+  transition: .3s;
+  font-size: 14pt;
+}
+
+/* =============================================
+
+Univeresal
+
+===============================================*/
+
+main {
+  min-height: calc(100vh - 157px);
+  width: 75%;
+  margin: 0 auto;
+}
+
+h1,
+h2,
+h3,
+h4 {
+  font-family: 'Source Code Pro',monospace;
+}
+
+h1,
+h2,
+h3,
+p {
+  color: #200d13;
 }
 
 h1 {
-  font-family: 'Source Code Pro', monospace;
+margin: 20px 0;
   font-size: 36pt;
-  padding: 20px 0;
+  font-weight: bold;
 }
 
-p {
+main p {
+  margin: 5% 0;
+  font-size: 12pt;
+}
+
+p,
+a,
+li,
+.label_input_wrap {
   font-family: 'Lato', sans-serif;
-  font-size: 14pt;
-  line-height: 2;
 }
 
-a {
-  color: #0021A5;
+main a {
   text-decoration: none;
+  color: #200d13;
   transition: .3s;
 }
 
 a:hover {
-  text-decoration: underline 2;
-  text-decoration-color: #0021A5;
+  border-bottom: solid 2pt #0021A5;
   transition: .3s;
 }
 
 a:active {
-  text-decoration: underline 2;
-  text-decoration-color: #E87722;
+  border-bottom: solid 2pt #E87722;
   transition: .3s;
 }
 
-.container {
-  width: 75%;
-  margin: auto;
+/* =============================================
+
+Footer Styling
+
+===============================================*/
+
+footer {
+  background-color: #8B1F41;
+}
+
+footer ul {
+  text-align: center;
+}
+
+footer ul li {
+  display: inline-block;
+  list-style: none;
+  color: white;
+  margin: 1% 10%;
+}
+
+footer ul li a {
+  color: white;
+  text-decoration: none;
+  transition: .3s;
 }
 
 </style>
 
-<div class="container">
+</head>
+<body>
+  <header>
+    <a href="index.html"><img src="images/logo.png" alt="kg logo" width="75" /></a>
+    <nav>
+      <ul>
+        <li><a href="work.html">Work</a></li>
+        <li><a href="resume.html">R&eacute;sum&eacute;</a></li>
+        <li><a href="contact.html">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+
+<main>
   <h1>thanks for reaching out</h1>
   <p>Your message has been received.</p>
-  <p><a href="index.html">Go Back to Page</a></p>
-</div>
+</main>
+
+<footer>
+  <ul>
+    <li><a href="mailto:georgi.kayce@ufl.edu">georgi.kayce@ufl.edu</a></li>
+    <li><a href="tel:2405294709">(240) 529-4709</a></li>
+    <li>Washington, DC</li>
+  </ul>
+</footer>
